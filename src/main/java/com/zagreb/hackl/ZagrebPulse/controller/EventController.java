8450f3f15496892c3829a70425f926a0759b3e6e
Nina Gnjidić <nina.gnjdic@fer.hr>
2024-11-23 16:11:25 +0100

    cors config

--- src/main/java/com/zagreb/hackl/ZagrebPulse/controller/EventController.java
+++ src/main/java/com/zagreb/hackl/ZagrebPulse/controller/EventController.java
@@ -3,6 +3,7 @@ package com.zagreb.hackl.ZagrebPulse.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +22,13 @@ public class EventController {
     }
 
     // Define the endpoint that will fetch events
+    @CrossOrigin(origins = {"http://localhost:4200"})
     @GetMapping("/api/events")
     public List<EventRecord> getEvents() {
         // Call the service to fetch events
         return eventService.fetchEvents(null);
     }
-
+    @CrossOrigin(origins = {"http://localhost:4200"})
     @GetMapping("/api/events/zabava")
     public List<EventRecord> getFunEvents() {
         // Call the service to fetch events
