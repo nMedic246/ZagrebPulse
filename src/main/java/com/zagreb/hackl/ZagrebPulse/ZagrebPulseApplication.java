538ae516501eadc24624929b5d89fc54477104e7
nMedic246 <nikamedic14@gmail.com>
2024-11-23 09:31:10 +0100

    init

---
+++ src/main/java/com/zagreb/hackl/ZagrebPulse/ZagrebPulseApplication.java
@@ -0,0 +1,13 @@
+package com.zagreb.hackl.ZagrebPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagrebPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZagrebPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zagreb/hackl/ZagrebPulse/ZagrebPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagreb.hackl.ZagrebPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagrebPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
